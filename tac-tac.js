fa7495ab3691760654a08c9c4e83a64a03f5e195
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-02-13 11:14:22 -0500

    added function to check win conditions

--- tac-tac.js
+++ tac-tac.js
@@ -17,7 +17,7 @@ function gameFlow () {
 
 //take P1's choice, check if valid, and update the array
 
-function playerOneUpdate () {
+function playerOneUpdate() {
     let oneChoice = prompt("What does player one choose?");
 
     if ((gameBoard[oneChoice] != "x") && (gameBoard[oneChoice] != "o")) {
@@ -29,6 +29,7 @@ function playerOneUpdate () {
 
     console.log(gameBoard);
 
+    checkWin();
 }
 
 //take P2's choice, check if valid, and update the array
@@ -44,33 +45,70 @@ function playerTwoUpdate() {
     };
 
     console.log(gameBoard);
+
+    checkWin();
 }
 
-playerOneUpdate();
-playerTwoUpdate();
-playerOneUpdate();
-playerTwoUpdate();
+
 
 //012
 //345
 //678
 
-function checkWin () {
-    if ((gameBoard[0] && gameBoard[1] && gameBoard[2]) == "x") {
+//check if P1 won the game
+
+function checkWin() {
+    if ((gameBoard[0] == "x") && (gameBoard[1]=="x") && (gameBoard[2]=="x")) {
         alert("player one wins!")
-    } else if ((gameBoard[3] && gameBoard[4] && gameBoard[5]) == "x") {
+    } else if ((gameBoard[3] == "x") && (gameBoard[4]=="x") && (gameBoard[5]=="x")) {
         alert("player one wins!")
-    } else if ((gameBoard[6] && gameBoard[7] && gameBoard[8]) == "x") {
+    } else if ((gameBoard[6] == "x") && (gameBoard[7]=="x") && (gameBoard[8]=="x")) {
         alert("player one wins!")
-    } else if ((gameBoard[0] && gameBoard[4] && gameBoard[8]) == "x") {
+    } else if ((gameBoard[0] == "x") && (gameBoard[4]=="x") && (gameBoard[8]=="x")) {
         alert("player one wins!")
-    } else if ((gameBoard[2] && gameBoard[4] && gameBoard[6]) == "x") {
+    } else if ((gameBoard[2] == "x") && (gameBoard[4]=="x") && (gameBoard[6]=="x")) {
         alert("player one wins!")
-    } 
+    } else if ((gameBoard[0] == "x") && (gameBoard[1]=="x") && (gameBoard[2]=="o")) {
+        alert("player two wins!")
+    } else if ((gameBoard[3] == "x") && (gameBoard[4]=="x") && (gameBoard[5]=="o")) {
+        alert("player two wins!")
+    } else if ((gameBoard[6] == "x") && (gameBoard[7]=="x") && (gameBoard[8]=="o")) {
+        alert("player two wins!")
+    } else if ((gameBoard[0] == "x") && (gameBoard[4]=="x") && (gameBoard[8]=="o")) {
+        alert("player two wins!")
+    } else if ((gameBoard[2] == "x") && (gameBoard[4]=="x") && (gameBoard[6]=="o")) {
+        alert("player two wins!")
+    }
+}
+
+function checkTwoWin() {
+    if ((gameBoard[0] && gameBoard[1] && gameBoard[2]) == "o") {
+        alert("player two wins!")
+    } else if ((gameBoard[3] && gameBoard[4] && gameBoard[5]) == "o") {
+        alert("player two wins!")
+    } else if ((gameBoard[6] && gameBoard[7] && gameBoard[8]) == "o") {
+        alert("player two wins!")
+    } else if ((gameBoard[0] && gameBoard[4] && gameBoard[8]) == "o") {
+        alert("player two wins!")
+    } else if ((gameBoard[2] && gameBoard[4] && gameBoard[6]) == "o") {
+        alert("player two wins!")
+    }  
 }
 
 
+function resetGame () {
+    gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+}
 
+
+playerOneUpdate();
+playerTwoUpdate();
+playerOneUpdate();
+playerTwoUpdate();
+playerOneUpdate();
+playerTwoUpdate();
+playerOneUpdate();
+playerTwoUpdate();
 
 
 
